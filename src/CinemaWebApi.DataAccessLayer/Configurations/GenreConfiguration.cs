c14d804fb874fc37dc1efe99f5c54f5482122b09
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-15 19:35:18 +0100

    added configurations for the entities

---
+++ src/CinemaWebApi.DataAccessLayer/Configurations/GenreConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using CinemaWebApi.DataAccessLayer.Configurations.Common;
+using CinemaWebApi.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CinemaWebApi.DataAccessLayer.Configurations
+{
+    internal sealed class GenreConfiguration : BaseEntityConfiguration<Genre>
+    {
+        public override void Configure(EntityTypeBuilder<Genre> builder)
+        {
+            base.Configure(builder);
+            builder.Property(genre => genre.Name).HasMaxLength(256).IsRequired();
+            builder.Property(genre => genre.Description).HasMaxLength(512);
+        }
+    }
+}
---
+++ src/CinemaWebApi.DataAccessLayer/Configurations/MovieConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CinemaWebApi.DataAccessLayer.Configurations.Common;
+using CinemaWebApi.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CinemaWebApi.DataAccessLayer.Configurations
+{
+    internal sealed class MovieConfiguration : BaseEntityConfiguration<Movie>
+    {
+        public override void Configure(EntityTypeBuilder<Movie> builder)
+        {
+            base.Configure(builder);
+            builder.Property(movie => movie.Title).HasMaxLength(256).IsRequired();
+            builder.Property(movie => movie.OriginalTitle).HasMaxLength(256).IsRequired();
+            builder.Property(movie => movie.ReleaseDate).IsRequired();
+            builder.Property(movie => movie.Language).HasMaxLength(50).IsRequired();
+            builder.Property(movie => movie.OriginalTitle).HasMaxLength(50).IsRequired();
+
+            builder.HasOne(movie => movie.Genre)
+                .WithMany(genre => genre.Movies)
+                .HasForeignKey(movie => movie.GenreId);
+        }
+    }
+}
