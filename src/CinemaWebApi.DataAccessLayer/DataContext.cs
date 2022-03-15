ff25c90cec2c28770ee22f568862f7f814686c65
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-15 20:34:19 +0100

    added IDataContext interface

--- src/CinemaWebApi.DataAccessLayer/DataContext.cs
+++ src/CinemaWebApi.DataAccessLayer/DataContext.cs
@@ -1,19 +1,54 @@
-﻿using CinemaWebApi.DataAccessLayer.Entities;
+﻿using CinemaWebApi.DataAccessLayer.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace CinemaWebApi.DataAccessLayer
 {
-    public class DataContext : DbContext
+    public class DataContext : DbContext, IDataContext
     {
         public DataContext(DbContextOptions<DataContext> options)
             : base(options)
         {
         }
 
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<Genre> Genres { get; set; }
+        public void Delete<T>(T entity) where T : BaseEntity
+        {
+            Set<T>().Remove(entity);
+        }
+        public void Edit<T>(T entity) where T : BaseEntity
+        {
+            Set<T>().Update(entity);
+        }
+        public async ValueTask<T> GetAsync<T>(params object[] keyValues) where T : BaseEntity
+        {
+            return await Set<T>().FindAsync(keyValues);
+        }
+        public void Insert<T>(T entity) where T : BaseEntity
+        {
+            Set<T>().Add(entity);
+        }
+        public async Task<bool> SaveAsync()
+        {
+            bool result;
+
+            try
+            {
+                await SaveChangesAsync();
+                result = true;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                result = false;
+            }
+            catch (DbUpdateException)
+            {
+                result = false;
+            }
+
+            return result;
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
