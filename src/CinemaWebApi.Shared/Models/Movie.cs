4062f62e1536c677a41f8aa23ff6b55ec27f99d9
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-15 21:05:49 +0100

    added response models

---
+++ src/CinemaWebApi.Shared/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CinemaWebApi.Shared.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public string OriginalTitle { get; set; }
+
+        public string Language { get; set; }
+
+        public string OriginalLanguage { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public Genre Genre { get; set; }
+    }
+}
