4e3c34e3d0db4590bb3a144668bcf91525dac6b0
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-15 21:18:39 +0100

    added RegisterGenreRequest class to manage registration of the Genre

---
+++ src/CinemaWebApi.Shared/Models/Requests/RegisterGenreRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaWebApi.Shared.Models.Requests
+{
+    public class RegisterGenreRequest
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
