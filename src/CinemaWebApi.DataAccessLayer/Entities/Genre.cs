902b2eb4d319c236f18cf4730e56ba8a93966c3c
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-15 19:35:04 +0100

    added Movie and Genre entities

---
+++ src/CinemaWebApi.DataAccessLayer/Entities/Genre.cs
@@ -0,0 +1,14 @@
+﻿using CinemaWebApi.DataAccessLayer.Entities.Common;
+using System.Collections.Generic;
+
+namespace CinemaWebApi.DataAccessLayer.Entities
+{
+    public class Genre : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ src/CinemaWebApi.DataAccessLayer/Entities/Movie.cs
@@ -0,0 +1,22 @@
+﻿using CinemaWebApi.DataAccessLayer.Entities.Common;
+using System;
+
+namespace CinemaWebApi.DataAccessLayer.Entities
+{
+    public class Movie : BaseEntity
+    {
+        public Guid GenreId { get; set; }
+
+        public string Title { get; set; }
+
+        public string OriginalTitle { get; set; }
+
+        public string Language { get; set; }
+
+        public string OriginalLanguage { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public virtual Genre Genre { get; set; }
+    }
+}
