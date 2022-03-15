3340aa84b1ef68461310db4cf9d8be10f8560fa0
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-15 19:34:41 +0100

    added BaseEntityConfiguration class

---
+++ src/CinemaWebApi.DataAccessLayer/Configurations/Common/BaseEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using CinemaWebApi.DataAccessLayer.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CinemaWebApi.DataAccessLayer.Configurations.Common
+{
+    internal abstract class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T>
+        where T : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(entity => entity.Id);
+            builder.Property(entity => entity.Id).ValueGeneratedOnAdd().IsRequired();
+        }
+    }
+}
