dfd2d1e76760d0cc53be1f6795acf0b4610bdb67
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-15 19:38:32 +0100

    added DataContext class

---
+++ src/CinemaWebApi.DataAccessLayer/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using CinemaWebApi.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection;
+
+namespace CinemaWebApi.DataAccessLayer
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Type dataContextType = typeof(DataContext);
+            Assembly currentAssembly = dataContextType.Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(currentAssembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
