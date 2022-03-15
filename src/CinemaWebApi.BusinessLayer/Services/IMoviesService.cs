e54071549147391bbd0e1ac2115a0d393fbcb4ab
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-15 21:06:33 +0100

    created MoviesService (the update must be completed)

---
+++ src/CinemaWebApi.BusinessLayer/Services/IMoviesService.cs
@@ -0,0 +1,15 @@
+﻿using CinemaWebApi.Shared.Models.Requests;
+using System.Threading.Tasks;
+
+using ApplicationMovie = CinemaWebApi.Shared.Models.Movie;
+
+namespace CinemaWebApi.BusinessLayer.Services
+{
+    public interface IMoviesService
+    {
+        Task<bool> AddMovieAsync(RegisterMovieRequest request);
+        Task<bool> DeleteMovieAsync(string title);
+        Task<ApplicationMovie> GetMovieAsync(string title);
+        Task<bool> UpdateMovieAsync();
+    }
+}
---
+++ src/CinemaWebApi.BusinessLayer/Services/MoviesService.cs
@@ -0,0 +1,71 @@
+﻿using CinemaWebApi.DataAccessLayer;
+using CinemaWebApi.DataAccessLayer.Entities;
+using CinemaWebApi.Shared.Models.Requests;
+using System.Threading.Tasks;
+
+using ApplicationGenre = CinemaWebApi.Shared.Models.Genre;
+using ApplicationMovie = CinemaWebApi.Shared.Models.Movie;
+
+namespace CinemaWebApi.BusinessLayer.Services
+{
+    public class MoviesService : IMoviesService
+    {
+        private readonly IDataContext dataContext;
+
+        public MoviesService(IDataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<bool> AddMovieAsync(RegisterMovieRequest request)
+        {
+            var genre = await dataContext.GetAsync<Genre>(request.Genre);
+            var movie = new Movie
+            {
+                Title = request.Title,
+                OriginalTitle = request.OriginalTitle,
+                Language = request.Language,
+                OriginalLanguage = request.OriginalLanguage,
+                ReleaseDate = request.ReleaseDate,
+                Genre = genre,
+            };
+
+            dataContext.Insert(movie);
+            return await dataContext.SaveAsync();
+        }
+        public async Task<bool> DeleteMovieAsync(string title)
+        {
+            var movie = await dataContext.GetAsync<Movie>(title);
+            if (movie == null)
+            {
+                return false;
+            }
+
+            dataContext.Delete(movie);
+            return await dataContext.SaveAsync();
+        }
+        public async Task<ApplicationMovie> GetMovieAsync(string title)
+        {
+            var movie = await dataContext.GetAsync<Movie>(title);
+            var genre = await dataContext.GetAsync<Genre>(movie.GenreId);
+            var applicationMovie = new ApplicationMovie
+            {
+                Title = movie.Title,
+                OriginalTitle = movie.OriginalTitle,
+                Language = movie.Language,
+                OriginalLanguage = movie.OriginalLanguage,
+                ReleaseDate = movie.ReleaseDate,
+                Genre = new ApplicationGenre
+                {
+                    Name = genre.Name,
+                    Description = genre.Description
+                }
+            };
+
+            return applicationMovie;
+        }
+
+        public Task<bool> UpdateMovieAsync()
+            => Task.FromResult(true);
+    }
+}
