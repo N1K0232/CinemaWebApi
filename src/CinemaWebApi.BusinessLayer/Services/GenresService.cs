4919b5feff0fce78113f38c038f82951b5cb9ba8
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-15 21:18:55 +0100

    added GenresService

---
+++ src/CinemaWebApi.BusinessLayer/Services/GenresService.cs
@@ -0,0 +1,55 @@
+﻿using CinemaWebApi.DataAccessLayer;
+using CinemaWebApi.DataAccessLayer.Entities;
+using CinemaWebApi.Shared.Models.Requests;
+using System.Threading.Tasks;
+
+using ApplicationGenre = CinemaWebApi.Shared.Models.Genre;
+
+namespace CinemaWebApi.BusinessLayer.Services
+{
+    public class GenresService : IGenresService
+    {
+        private readonly IDataContext dataContext;
+
+        public GenresService(IDataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<bool> AddGenreAsync(RegisterGenreRequest request)
+        {
+            var genre = new Genre
+            {
+                Name = request.Name,
+                Description = request.Description,
+            };
+
+            dataContext.Insert(genre);
+            return await dataContext.SaveAsync();
+        }
+        public async Task<bool> DeleteGenreAsync(string name)
+        {
+            var genre = await dataContext.GetAsync<Genre>(name);
+            if (genre == null)
+            {
+                return false;
+            }
+
+            dataContext.Delete(genre);
+            return await dataContext.SaveAsync();
+        }
+        public async Task<ApplicationGenre> GetGenreAsync(string name)
+        {
+            var genre = await dataContext.GetAsync<Genre>(name);
+            var applicationGenre = new ApplicationGenre
+            {
+                Name = genre.Name,
+                Description = genre.Description
+            };
+
+            return applicationGenre;
+        }
+        public Task<bool> UpdateGenreAsync()
+            => Task.FromResult(true);
+    }
+}
---
+++ src/CinemaWebApi.BusinessLayer/Services/IGenresService.cs
@@ -0,0 +1,14 @@
+﻿using CinemaWebApi.Shared.Models;
+using CinemaWebApi.Shared.Models.Requests;
+using System.Threading.Tasks;
+
+namespace CinemaWebApi.BusinessLayer.Services
+{
+    public interface IGenresService
+    {
+        Task<bool> AddGenreAsync(RegisterGenreRequest request);
+        Task<bool> DeleteGenreAsync(string name);
+        Task<Genre> GetGenreAsync(string name);
+        Task<bool> UpdateGenreAsync();
+    }
+}
